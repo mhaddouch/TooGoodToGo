b5c72dc645985c4fac35a3212095d8c42091570e
mhaddouc1 <m.haddouch1@student.avans.nl>
2023-10-12 00:55:03 +0200

    login scherm aangemaakt

--- Portal/Controllers/AccountController.cs
+++ Portal/Controllers/AccountController.cs
@@ -1,40 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Portal.Models;
-using System.Diagnostics;
 
 namespace Portal.Controllers
 {
-    public class HomeController : Controller
+    public class AccountController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Index(StudentViewModel studentViewModel)
         {
-           if(ModelState.IsValid)
+            if (ModelState.IsValid)
             {
                 return View("HomeScreen");
             }
-            else { return View(); } 
-            
+            else { return View(); }
         }
-
-        public IActionResult HomeScreen()
+        public IActionResult Login()
         {
             return View();
         }
-        
-
-
     }
 }
